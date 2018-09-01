1cd37ed278b5d8cb85443046024c5b8e8fa25b06
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-01 18:29:34 +0300

    dictionaty function with values as the square number for their keys

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,14 @@
-#variable for the empty dictionary
-sq_number = dict()
-
 #function to generate the dictionary keys and values
-def dictinary():
-    #create dictionary keys as numbers with values as their squares
-    for n in range(1,16):
-        sq_number[n] = n**2
-    return sq_number
+def dictinary(a,b):
+    if type(a) == int and type(b) == int:
+        #variable for the empty dictionary
+        sq_number = dict()
+        #create dictionary keys as numbers with values as their squares
+        for n in range(a,b):
+            sq_number[n] = n**2
+        return sq_number
+    else:
+        return "Please use numbers(integers)"
 
 #print the generated dictinary
-print dictinary()
+print dictinary('r',15)
