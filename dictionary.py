4881119f9716c7d698f8060475aec1d38a795b38
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-04 19:40:22 +0300

    replace the loop by using comprehension

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,12 @@
 #function to generate the dictionary keys and values
 def dictinary(a,b):
     if type(a) == int and type(b) == int:
-        #variable for the empty dictionary
-        sq_number = dict()
-        #create dictionary keys as numbers with values as their squares
-        for n in range(a,b):
-            sq_number[n] = n**2
+        #using the concept of comprehension
+        #lets create a dictionary with keys as numbers with values as their squares
+        sq_number = {n:n**2 for n in range(a,b)}
         return sq_number
     else:
         return "Please use numbers(integers)"
 
 #print the generated dictinary
-print dictinary('r',15)
+print dictinary(4,15)
