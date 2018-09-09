4b3d56556eb69229d8d90ea6319ae6b0c3f7181e
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-09 18:17:11 +0300

    use print in built function

--- dictionary.py
+++ dictionary.py
@@ -5,8 +5,8 @@ def dictinary(a,b):
         #lets create a dictionary with keys as numbers with values as their squares
         sq_number = {n:n**2 for n in range(a,b)}
         return sq_number
-    else:
-        return "Please use numbers(integers)"
+    
+    return "Please use numbers(integers)"
 
 #print the generated dictinary
-print dictinary(4,15)
+print(dictinary(4,15))
